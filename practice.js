5e1cca2f27623970f6d136bdeaffd8c867fa1ad9
liv0161 <olivia777c@icloud.com>
2026-02-06 09:53:42 +0000

    Add score tracking and feedback message
    Fix typo in feedback text assignment and implement score tracking.

--- practice.js
+++ practice.js
@@ -1,1 +1,7 @@
-
+let score = 0;
+function submitAnswer() {
+  score ++;
+  localStorage.setItem("score",score);
+  document.getElementById("feedback").inneText =
+    "Answer submitted! Your score is currently " + score;
+}
