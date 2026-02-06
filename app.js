1a26e981401898f46a44128e590ab72db9ce9126
liv0161 <olivia777c@icloud.com>
2026-02-06 12:18:38 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,4 +2,11 @@ function checkSession() {
   const user = localStorage.getItem("currentUser);
   if (!user) {
      winndow.location.href = "index.html";
+  }
+}
+
+function logout(){
+  localStorage.removeItem("currentUser");
+  window.location.href = "index.html";
+}
     
