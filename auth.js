799e57fb108b955165991ef5ef98e69097954e03
liv0161 <olivia777c@icloud.com>
2026-02-06 09:34:29 +0000

    Add login function to handle user authentication

--- auth.js
+++ auth.js
@@ -1,1 +1,11 @@
+function login(){
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value
 
+  if (email && password){
+    localStorage.setItem("user",email);
+    alert("Login successful");
+  } else {
+    alert("please enter email and password");
+  }
+}
