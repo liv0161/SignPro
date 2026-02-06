7f765b88af74eeb76a78af05afdf1485f441aa1e
liv0161 <olivia777c@icloud.com>
2026-02-06 09:51:12 +0000

    Add lesson display functionality to learn.js

--- learn.js
+++ learn.js
@@ -1,1 +1,21 @@
 
+
+
+
+
+
+
+
+
+const container=document.getElementById("lessonContainer");
+lessons.forEach(lesson => {
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <h3>${lesson.title}</h3>
+    <p>${lesson.description}</p>
+    <a href="${lesson.link}" target="_blank">
+      View sign on UCL SignBank
+    </a>
+  `;
+  container.appendChild(div);
+});
