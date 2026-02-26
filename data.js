f984386aa3140d9f4bc3953a27d9fde31fa2f252
liv0161 <olivia777c@icloud.com>
2026-02-26 12:51:49 +0000

    Create course data structure with lessons and signs
    Added a course structure with lessons and signs for greetings and polite phrases.

---
+++ data.js
@@ -0,0 +1,39 @@
+const course = [
+  {
+    id:"unit1",
+    title:"Greetings",
+    lessons: [
+      {
+        id: "lesson1",
+        title: "Basic Greetings",
+        signs: [
+          {
+            name: "Hello",
+            explanation: " A general greeting.",
+            embed: "https://bslsignbank.ucl.ac.uk/media/bsl-video/WA/WAVE-HAND.mp4"
+          },
+          {
+            name: "Goodbye",
+            explanation: " Used when leaving.",
+            embed: "https://bslsignbank.ucl.ac.uk/media/bsl-video/WA/WAVE-HAND.mp4"
+          }
+          ]
+      },
+      {
+        id: "lesson2",
+        title:"Polite Phrases",
+        signs:[
+          {
+            name:"Thank you",
+            explanation:"used to show gratitude.",
+            embed: "https://bslsignbank.ucl.ac.uk/media/bsl-video/TH/THANK.mp4"
+          }
+        ]
+      }
+    ]
+  }
+];
+
+
+       
+            
