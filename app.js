a24c5c2955ce81dc3866e71c59fc283ebe18566c
liv0161 <olivia777c@icloud.com>
2026-02-26 22:08:33 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 function checkSession() {
-  const user = localStorage.getItem("currentUser);
-  if (!user) {
-     winndow.location.href = "index.html";
+  if (!localStorage.getItem("currentUser")){
+    window.location.href = "index.html";
   }
 }
-
 function logout(){
   localStorage.removeItem("currentUser");
   window.location.href = "index.html";
 }
-    
+
+function updteStreak(){
+  const today = new Date().toDateString();
+  const last = localStorage.getItem("lastActive");
+  let streak = Number(localStorage.getItem("streak")) || 0;
+
+  if (last!== today){
+    streak++;
+    localStorage.setItem("streak",streak);
+    localStorage.setItem("lastActive",today);
+  }
+}
