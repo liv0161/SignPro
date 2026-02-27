064422f85bafd5f2a02874bc98fd05a841af03ed
liv0161 <olivia777c@icloud.com>
2026-02-27 09:09:36 +0000

    Refactor loadSign and finishLesson functions

--- Lesson.js
+++ Lesson.js
@@ -1,12 +1,38 @@
-const lessonId = localStorage.getItem("currentLesson");
+  const lessonId = localStorage.getItem("currentLesson");
 const lessons = course[0].lessons;
-const lesson = lessons.find(1=> 1.id == lessonID);
+const lesson=lessons.find(1 => 1.id=== lessonId);
+
 
 let index = 0;
-const completed=
-  JSON.parse(localStorage.getItem("completedLessons")|| [];
-document.getElementById("lessonTitle").innerText= lesson.title;
+const completed =
+  JSON.parse(localStorage.getItem("completedLessons")) || [];
+document.getElementById("lessonTitle").innerText = lesson.title;
 
 function loadSign(){
   const sign = lesson.signs[index];
-  document.getElementById("counter").innerText = 
+  document.getElementById("counter").innerText =
+    `Sign ${index+1} of ${lesson.signs.length}`;
+  document.getElementById("signName").innerText = sign.name;
+  document.getElementById("signExplanation").innerText = sign.explanation;
+  document.getElementById("frame").src=sign.embed;
+}
+
+function next(){
+  index++;
+  if (index < lesson.signs.length){
+    loadSign();
+  } else{
+    finishLeson();
+  }
+}
+  
+function finishLesson(){
+  if (!completed.includes(lesson.id)){
+    completed.push(lesson.id);
+    localStorage.setItem("completedLessons",JSON.stringify(completed));
+    updateStreak();
+  }
+
+  document.getElementById("status").innerText = "Lesson completed";
+}
+loadSign();
