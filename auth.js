346a61c5c5fed9c86bbe72d5a7603bdc9aea4a28
liv0161 <olivia777c@icloud.com>
2026-03-05 12:15:27 +0000

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,10 +1,29 @@
-alert("auth.js is loading");
-
+function getUsers() {
+ return JSON.parse(localStorage.getItem("users")) || {};
+}
 function register() {
-  alert("Register works");
+ const email = emailInput();
+ const password = passwordInput();
+ const users = getUsers();
+ if (users[email]) return alert("User already exists");
+ users[email] = password;
+ localStorage.setItem("users", JSON.stringify(users));
+ alert("Account created");
 }
-
 function login() {
-  alert("Login works");
+ const email = emailInput();
+ const password = passwordInput();
+ const users = getUsers();
+ if (users[email] === password) {
+   localStorage.setItem("currentUser", email);
+   window.location.href = "course.html";
+ } else {
+   alert("Invalid login");
+ }
 }
-
+function emailInput() {
+ return document.getElementById("email").value;
+}
+function passwordInput() {
+ return document.getElementById("password").value;
+}
